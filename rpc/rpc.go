f8ce6ca461ca3f47f02b555c081fed6a8ce74770
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2024-04-18 20:26:17 +0100

    till logger

---
+++ rpc/rpc.go
@@ -0,0 +1,63 @@
+package rpc
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+func EncodeMessage(msg any) string {
+	content, err := json.Marshal(msg)
+
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("Content-Lenght: %d\r\n\r\n%s", len(content), content)
+}
+
+type BaseMessage struct {
+	Method string `json:"method"`
+}
+
+func DecodeMessage(msg []byte) (string, []byte, error) {
+	header, content, found := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
+	if !found {
+		return "", nil, errors.New("Did not find separator")
+	}
+	// Content-Lengtg: <number>
+	contentLenghtBytes := header[len("Content-Length: "):]
+	contentLength, err := strconv.Atoi(string(contentLenghtBytes))
+	if err != nil {
+		return "", nil, err
+	}
+	// TODO: We will get to this
+	var baseMessage BaseMessage
+	contentB := content[:contentLength]
+	if err := json.Unmarshal(contentB, &baseMessage); err != nil {
+		return "", nil, err
+	}
+	return baseMessage.Method, contentB, nil
+}
+
+// type SplitFunc func(data []byte, atEOF bool) (advance int, token []byte, err error)
+func Split(data []byte, _ bool) (advance int, token []byte, err error) {
+
+	header, content, found := bytes.Cut(data, []byte{'\r', '\n', '\r', '\n'})
+	if !found {
+		return 0, nil, nil // Not a problem, we just need more data
+	}
+	// Content-Lengtg: <number>
+	contentLenghtBytes := header[len("Content-Length: "):]
+	contentLength, err := strconv.Atoi(string(contentLenghtBytes))
+	if err != nil {
+		return 0, nil, err
+	}
+	if contentLength < len(content) {
+		return 0, nil, nil // Not a problem, we just need more data
+	}
+	totalLenght := len(header) + 4 + contentLength
+	// TODO: We will get to this
+	return totalLenght, data[:totalLenght], nil
+}
---
+++ rpc/rpc_test.go
@@ -0,0 +1,36 @@
+package rpc_test
+
+import (
+	"educationalsp/rpc"
+	"testing"
+)
+
+type EncondigExample struct {
+	Testing bool
+}
+
+func TestEncode(t *testing.T) {
+	expected := "Content-Lenght: 16\r\n\r\n{\"Testing\":true}"
+	actual := rpc.EncodeMessage(EncondigExample{Testing: true})
+	if expected != actual {
+		t.Fatalf("Expected: %s, Actual: %s", expected, actual)
+	}
+}
+
+func TestDecode(t *testing.T) {
+	incomingMessage := "Content-Lenght: 15\r\n\r\n{\"Method\":\"hi\"}"
+
+	method, content, err := rpc.DecodeMessage([]byte(incomingMessage))
+
+	contentLength := len(content)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if contentLength != 15 {
+		t.Fatalf("Expected: %d, Actual: %d", 15, contentLength)
+	}
+	if method != "hi" {
+		t.Fatalf("Expected: 'hi', Got: %s", method)
+	}
+}
